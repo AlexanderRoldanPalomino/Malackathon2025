7189c3b70e2d5b6cd27e42d4684350baf1377bb8
AlexanderRoldanPalomino <139011227+AlexanderRoldanPalomino@users.noreply.github.com>
2025-10-16 11:07:20 +0200

    Added Main to gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // Hola
         Malackathon malackathon = new Malackathon("D:/Respositories v2/GitHub - AlexanderRoldanPalomino/Malackathon2025/wallet");
         malackathon.init();
     }
